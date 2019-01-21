c29b82652ba187bd5aa44e6df2059b8bd8ccdde0
Brian Lach <brianlach72@gmail.com>
2019-01-21 11:05:15 -0800

    Move libp3pystub when doing static build

--- compile_static.bat
+++ compile_static.bat
@@ -1,4 +1,6 @@
 @echo off
 set PATH=%systemroot%\System32;%PATH%
+move built_x64_static\libp3pystub.lib built_x64_static\lib\libp3pystub.lib
 thirdparty\win-python-x64\python makepanda/makepanda.py --everything --no-touchinput --verbose --distributor="Cog Invasion Online" --threads=3 --optimize=3 --arch=x64 --windows-sdk=10 --msvc-version=14.1 --no-maya2015 --no-maya2017 --outputdir built_x64_static --static %*
+move built_x64_static\lib\libp3pystub.lib built_x64_static\libp3pystub.lib
 pause
