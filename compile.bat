ffac1f0e3a69ff5f267f132fc056370075fd2706
Brian Lach <brianlach72@gmail.com>
2019-07-08 13:47:28 -0400

    simpleHashMap: fix compile error

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 @echo off
 set PATH=%systemroot%\System32;%PATH%
-thirdparty\win-python-x64\python makepanda/makepanda.py --everything --no-touchinput --verbose --distributor="Cog Invasion Online" --threads=6 --optimize=3 --arch=x64 --windows-sdk=10 --msvc-version=14.1 --no-maya2015 --no-maya2017
+thirdparty\win-python-x64\python makepanda/makepanda.py --everything --no-touchinput --verbose --distributor="Cog Invasion Online" --threads=5 --optimize=3 --arch=x64 --windows-sdk=10 --msvc-version=14.1 --no-maya2015 --no-maya2017
 pause
